5deed638ce6c0b33ffff2f8468b1e64958bf64ac
Sinan Talha KOŞAR <4248869-sinantalhakosar@users.noreply.gitlab.com>
2020-12-18 02:16:11 +0300

    home, register, login pages added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET users register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register');
 });
 
 module.exports = router;
